b030fa3b64c550d41125316f7e90c3316999a23e
uttombarmon <uttombarmondev@gmail.com>
2026-01-17 21:45:15 +0600

    day's 2

---
+++ Days_2/comments_and_variables.py
@@ -0,0 +1,14 @@
+# this is single line comment example
+
+'''
+this is multiline comment example
+2nd line
+3rd line
+4th line
+'''
+
+#variable examples
+roll = 1
+name = "Ex Student"
+cgpa = 3.21
+isStudent = true
